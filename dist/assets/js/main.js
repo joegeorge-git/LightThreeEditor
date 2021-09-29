74f3bf8eef28e1b4bdab60af5185cecd379c4c37
Joe George <tech@dreamkatcher.com>
2021-09-29 19:59:13 +0530

    transform controlls added

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -6,10 +6,10 @@ import { GLTFExporter } from './GLTFExporter.js';
 import { TransformControls } from './TransformControls.js';
 import { DragControls } from './DragControls.js';
 
-var renderer, scene, camera, manager, controls;
-const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
-
+var renderer, scene, camera, manager, controls,group , orbit , control;
+const mouse = new THREE.Vector2(),
+raycaster = new THREE.Raycaster();
+let enableSelection = false;
 
 class LightThreeEditor {
     renderer;
@@ -53,11 +53,22 @@ class LightThreeEditor {
         manager = new THREE.LoadingManager(this.viewRender);
         const axesHelper = new THREE.AxesHelper(2);
         scene.add(axesHelper);
+        
+        group = new THREE.Group();
+		scene.add( group );
 
-        const material = new THREE.LineBasicMaterial({
-            color: 0x0000ff
-        });
+        orbit = new OrbitControls( camera, renderer.domElement );
+        orbit.update();
+        orbit.addEventListener( 'change', this.viewRender );
 
+        control = new TransformControls( camera, renderer.domElement );
+        control.addEventListener( 'change', this.viewRender );
+
+        control.addEventListener( 'dragging-changed', function ( event ) {
+            orbit.enabled = ! event.value;
+        } );
+
+        this.enableHotkeys()
         this.viewRender()
         this.itemsLoaderScreen(false)
     }
@@ -103,12 +114,112 @@ class LightThreeEditor {
             mesh.position.y = 0;
             scene.add(mesh);
             self.objects.push(mesh);
-            self.viewRender()
+            
             console.log('loaded')
+           
+            control.attach( mesh );
+            scene.add( control );
+           
             self.itemsLoaderScreen(false)
         });
     }
+    enableHotkeys(){
+        window.addEventListener( 'keydown', function ( event ) {
 
+            switch ( event.keyCode ) {
+
+                case 81: // Q
+                    control.setSpace( control.space === 'local' ? 'world' : 'local' );
+                    break;
+
+                case 16: // Shift
+                    control.setTranslationSnap( 100 );
+                    control.setRotationSnap( THREE.MathUtils.degToRad( 15 ) );
+                    control.setScaleSnap( 0.25 );
+                    break;
+
+                case 87: // W
+                    control.setMode( 'translate' );
+                    break;
+
+                case 69: // E
+                    control.setMode( 'rotate' );
+                    break;
+
+                case 82: // R
+                    control.setMode( 'scale' );
+                    break;
+
+                case 67: // C
+                    const position = currentCamera.position.clone();
+
+                    currentCamera = currentCamera.isPerspectiveCamera ? cameraOrtho : cameraPersp;
+                    currentCamera.position.copy( position );
+
+                    orbit.object = currentCamera;
+                    control.camera = currentCamera;
+
+                    currentCamera.lookAt( orbit.target.x, orbit.target.y, orbit.target.z );
+                    onWindowResize();
+                    break;
+
+                case 86: // V
+                    const randomFoV = Math.random() + 0.1;
+                    const randomZoom = Math.random() + 0.1;
+
+                    cameraPersp.fov = randomFoV * 160;
+                    cameraOrtho.bottom = - randomFoV * 500;
+                    cameraOrtho.top = randomFoV * 500;
+
+                    cameraPersp.zoom = randomZoom * 5;
+                    cameraOrtho.zoom = randomZoom * 5;
+                    onWindowResize();
+                    break;
+
+                case 187:
+                case 107: // +, =, num+
+                    control.setSize( control.size + 0.1 );
+                    break;
+
+                case 189:
+                case 109: // -, _, num-
+                    control.setSize( Math.max( control.size - 0.1, 0.1 ) );
+                    break;
+
+                case 88: // X
+                    control.showX = ! control.showX;
+                    break;
+
+                case 89: // Y
+                    control.showY = ! control.showY;
+                    break;
+
+                case 90: // Z
+                    control.showZ = ! control.showZ;
+                    break;
+
+                case 32: // Spacebar
+                    control.enabled = ! control.enabled;
+                    break;
+
+            }
+
+        } );
+
+        window.addEventListener( 'keyup', function ( event ) {
+
+            switch ( event.keyCode ) {
+
+                case 16: // Shift
+                    control.setTranslationSnap( null );
+                    control.setRotationSnap( null );
+                    control.setScaleSnap( null );
+                    break;
+
+            }
+
+        } );
+    }
     viewRender() {
         renderer.render(scene, camera);
     }
